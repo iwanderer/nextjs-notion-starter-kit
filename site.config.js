72acee54d98b0010cd27064336a523f472edef9e
iwanderer <iwanderer@users.noreply.github.com>
2021-06-19 20:14:29 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,24 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'Front-End-fbb9f404ad3140e1b0ce3e8b21987e9e',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Front End',
+  domain: 'https://www.notion.so/iwanderer/Front-End-fbb9f404ad3140e1b0ce3e8b21987e9e',
+  author: 'iwanderer',
 
   // open graph metadata (optional)
-  description: 'Example site description',
+  description: 'All About Front End',
   socialImageTitle: 'Transitive Bullshit',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'iwanderer',
+  github: 'iwanderer',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
